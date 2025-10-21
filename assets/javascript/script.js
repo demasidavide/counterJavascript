fa9af41b8c73b8b90f0267b76b1b3c098f295eb7
Davide De Masi <demasi.dav@gmail.com>
2025-10-21 16:29:55 +0200

    creato interfaccia completa

---
+++ assets/javascript/script.js
@@ -0,0 +1,82 @@
+let number=0;
+
+const generate= document.querySelector('#generate');
+generate.addEventListener('click', ()=>{
+    // controllo per vedere se la finestra esiste gia
+    if(!document.querySelector('.container-row-down')){
+    // creo div container figlio
+    const nuovoDiv = document.createElement('div');
+    nuovoDiv.classList.add('container-row-down');
+    document.body.appendChild(nuovoDiv);
+    // creo div counter figlio
+    const counter=document.createElement('div');
+    counter.classList.add('counter');
+    nuovoDiv.appendChild(counter);
+    // creo div display figlio
+    const display=document.createElement('div');
+    display.classList.add('display');
+    display.textContent=number;
+    counter.appendChild(display);
+    // creo div row-button fratello di display
+    const rowButton= document.createElement('div');
+    rowButton.classList.add('row-button');
+    counter.appendChild(rowButton);
+    // creo bottoni figli e fratelli fra loro
+    const btnP=document.createElement('button');
+    btnP.id='plus';
+    btnP.textContent='+';
+    rowButton.appendChild(btnP);
+
+    // funzione aggiungi numero
+    btnP.addEventListener('click',()=>{
+        number++;
+        display.textContent=number;
+     });   
+
+    const btnM=document.createElement('button');
+    btnM.id='plus';
+    btnM.textContent='-';
+    rowButton.appendChild(btnM);
+    // funzione sottrai numero
+    btnM.addEventListener('click', ()=>{
+        number--;
+        display.textContent=number;
+    });
+    // creo riga per colori
+    const rowSpan= document.createElement('div');
+    rowSpan.classList.add('row-color');
+    nuovoDiv.append(rowSpan);
+    // creo 3 span per colori
+    const span1=document.createElement('span');
+    span1.classList.add('color1');
+    span1.textContent= 'Colore 1'
+    rowSpan.appendChild(span1);
+    span1.addEventListener('click',()=>{
+        counter.className= 'counter';
+    });
+
+    const span2=document.createElement('span');
+    span2.classList.add('color2');
+    span2.textContent= 'Colore 2';
+    rowSpan.appendChild(span2);
+    span2.addEventListener('click',()=>{
+        counter.className= 'counter-2'
+    });
+
+    const span3=document.createElement('span');
+    span3.classList.add('color3');
+    span3.textContent= 'Colore 3'
+    rowSpan.appendChild(span3);
+    span3.addEventListener('click',()=>{
+        counter.className= 'counter-3'
+    })
+
+    }else{
+        alert('Elemento gia creato');
+    }
+
+     
+
+
+
+})
