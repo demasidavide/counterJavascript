3155b62aae068a4521a502baee9bcfb7c717232e
Davide De Masi <demasi.dav@gmail.com>
2025-10-30 10:28:39 +0100

    sostituito creazione elementi con nuova funzione

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,43 +1,50 @@
-let number=0; //inizializzo variabile iniziale counter
+//inizializzo variabile iniziale counter
+let number=0; 
 
-function createChild(name,type,className,parent){
+//funzione per creare elementi figli
+function createChildSibling(name,type,className,parent){ 
     name= document.createElement(type);
     name.classList.add(className);
     parent.appendChild(name);
     return name;
 }
 
+
 const generate= document.querySelector('#generate');
 generate.addEventListener('click', ()=>{
     // controllo per vedere se la finestra esiste gia
     if(!document.querySelector('.container-row-down')){
     // creo div container figlio
-    const nuovoDiv = document.createElement('div');
-    nuovoDiv.classList.add('container-row-down');
-    document.body.appendChild(nuovoDiv);
+    const nuovoDiv=createChildSibling('nuovoDiv','div','container-row-down',document.body);
+        // const nuovoDiv = document.createElement('div');
+        // nuovoDiv.classList.add('container-row-down');
+        // document.body.appendChild(nuovoDiv);
     // creo div counter figlio
-    const counter=document.createElement('div');
-    counter.classList.add('counter');
-    nuovoDiv.appendChild(counter);
-
-    //prova con funzione
-       const display= createChild('display','div','display',counter);
+    const counter=createChildSibling('counter','div','counter',nuovoDiv)
+        //const counter=document.createElement('div');
+        //counter.classList.add('counter');
+        //nuovoDiv.appendChild(counter);
+    // creo div display figlio
+       const display= createChildSibling('display','div','display',counter);
        display.textContent=number;
 
-    // creo div display figlio
+    
         // const display=document.createElement('div');
         // display.classList.add('display');
         // display.textContent=number;
         // counter.appendChild(display);
     // creo div row-button fratello di display
-    const rowButton= document.createElement('div');
-    rowButton.classList.add('row-button');
-    counter.appendChild(rowButton);
+    const rowButton=createChildSibling('rowButton','div','row-button',counter)
+    // const rowButton= document.createElement('div');
+    // rowButton.classList.add('row-button');
+    // counter.appendChild(rowButton);
+
     // creo bottoni figli e fratelli fra loro
-    const btnP=document.createElement('button');
-    btnP.id='plus';
+    const btnP=createChildSibling('btnP','button','#plus',rowButton);
+    // const btnP=document.createElement('button');
+    // btnP.id='plus';
     btnP.textContent='+';
-    rowButton.appendChild(btnP);
+    // rowButton.appendChild(btnP);
     // funzione aggiungi numero
     btnP.addEventListener('click',()=>{
         number++;
